ecd3c2380a3fd55ec2d7894638a0e31bf7f26639
Holger Grosse-Plankermann <holgergp@gmail.com>
2021-12-14 18:02:29 +0100

    Redux Inc

---
+++ reduxInc.js
@@ -0,0 +1,55 @@
+function createStore(reducer, initialState) {
+    let currentReducer = reducer;
+    let currentState = initialState;
+    let listener = () => {};
+  
+    function getState() {
+      return currentState;
+    }
+    function dispatch(action) {
+      currentState = currentReducer(currentState, action);
+      listener();
+      return action;
+    }
+    function subscribe(newListener) {
+      listener = newListener;
+    }
+    return {
+      getState: getState,
+      dispatch: dispatch,
+      subscribe: subscribe
+    };
+  }
+  
+  function counter(state, action) {
+    const stateInitialized = isNaN(state)?0:state;
+    switch (action.type) {
+      case "INCREMENT":
+        return stateInitialized + 1;
+      case "DECREMENT":
+        return stateInitialized - 1;
+      default:
+        return stateInitialized;
+    }
+  }
+  
+  const store = createStore(counter);
+  
+  store.subscribe(() => console.log(store.getState()));
+  
+  //store.dispatch({ type: "INCREMENT" });
+  //store.dispatch({ type: "INCREMENT" });
+  //store.dispatch({ type: "DECREMENT" });
+  
+  
+  Bangle.on("tap", () => {
+    store.dispatch({ type: "INCREMENT" });
+    g.reset().clearRect(Bangle.appRect);
+    g.setFont("12x20").setFontAlign(0,0);
+    g.drawString("Redux Inc", 80, 70);
+    g.setFont("Vector:40");
+    g.drawString(store.getState(), 80,105);
+  });
+  
+  Bangle.loadWidgets();
+  Bangle.drawWidgets();
